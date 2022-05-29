44d2ed92e16bf2a9e3d4c54a607379e98608280f
Peter-Schenkels <peter.schenkels@student.hu.nl>
2022-05-30 01:09:48 +0200

    make GreedyMesh grouping algorithm

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+
+Servo servo;
+
+void setup() {
+  servo.attach(9);
+  
+}
+void loop() {
+  servo.write(0);
+  delay(500);
+  delay(10000);
+  servo.write(90);
+  delay(500);
+  delay(10000);
+  servo.write(180);
+  delay(500);
+  delay(10000);
+  servo.write(90);
+  delay(500);
+  delay(10000);
+  servo.write(0);
+  delay(500);
+  delay(10000);
+}
